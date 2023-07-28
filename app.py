51c0d195487ad93dc50b8b29871936a0ae478bda
Ashwani kumar <rathiashwani8@gmail.com>
2023-07-28 14:41:18 +0530

    This is my calculator

---
+++ app.py
@@ -0,0 +1,25 @@
+def calculator():
+    num1 = float(input("Enter the first number"))
+    num2 = float(input("Enter the second number"))
+
+#Addition 
+
+    result = num1 + num2
+    print("The sum of two number is: ", result)
+
+# Subtract
+
+    result = num1 - num2
+    print("The differnce of the two number is: ", result)
+
+# Multiplication
+
+    result = num1 * num2
+    print("The Multiplication of the two number is: ", result)
+
+# Divide 
+
+    result = num1 / num2
+    print("The Divide of the two number is: ", result)
+
+calculator()
